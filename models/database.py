fda6684d7d8f8478846f8a8fc8f00ee1a219e272
“westley” <westleyvieirasilva5@gmail.com>
2026-05-31 00:39:59 -0300

    fix: postgres render connection

--- models/database.py
+++ models/database.py
@@ -1,21 +1,9 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-
 def get_connection():
-    conn = psycopg2.connect(
-        dbname="gostock",
-        user="macbookpro",
-        host="localhost"
+    return psycopg2.connect(
+        os.environ["DATABASE_URL"],
+        cursor_factory=RealDictCursor
     )
-    return conn
-
-
-def inicializar_banco():
-    from models.usuario import Usuario
-    from models.produto import Produto
-
-    Usuario.criar_tabela()
-    Produto.criar_tabela()
-
-    print("✅ Banco PostgreSQL inicializado com sucesso.")
